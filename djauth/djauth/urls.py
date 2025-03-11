f42a8057a9fe4b00e9fd076f5b9c9cb01b19c56e
varun <varunsahil10@gmail.com>
2025-03-11 23:34:55 +0530

    Fix urls

--- djauth/djauth/urls.py
+++ djauth/djauth/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from home import views as home_views
+from home import urls as home_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include(home_views)),
+    path('',include(home_urls)),
 ]
